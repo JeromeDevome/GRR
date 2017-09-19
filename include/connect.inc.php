eb08fae442bb4ee01985efab298a4098f783e913
Philippe Boissonneault <p.boissonneault@gmail.com>
2017-09-19 15:40:14 -0400

    * Correction des changements d'heures Québec vs France (amérique vs europe) * Correction des journées perdues dans les semaines de changement d'heure dans le calendrier d'une ressource (week.php) - Revoir le ldap_search pour les groupes (functions.inc.php) - Retrait de tout ce qui est en lien avec l'heure d'été ou normale (parce ce que ce n'est plus nécessaire)

--- include/connect.inc.php
+++ include/connect.inc.php
@@ -1,35 +1,13 @@
 <?php
-# Les quatre lignes suivantes sont à modifier selon votre configuration
-# ligne suivante : le nom du serveur qui herberge votre base sql.
-# Si c'est le même que celui qui heberge les scripts, mettre "localhost"
-switch($_SERVER['SERVER_NAME']) {
-    case 'grr-bkp.web-dev.flsh.usherbrooke.ca':
-    case 'grr-svn.web-dev.flsh.usherbrooke.ca':
-    case 'grr-git.web-dev.flsh.usherbrooke.ca':
-    case 'grr.web-dev.flsh.usherbrooke.ca':
-        $dbHost="sgbd1.flsh.usherbrooke.ca";
-        # ligne suivante : le nom de votre base sql
-        $dbDb="grr_dev";
-        # ligne suivante : le nom de l'utilisateur sql qui a les droits sur la base
-        $dbUser="GRRmusique";
-        # ligne suivante : le mot de passe de l'utilisateur sql ci-dessus
-        $dbPass="music2013";
-        # ligne suivante : préfixe du nom des tables de données
-        $table_prefix="";
-        # ligne suivante : Port MySQL laissé par défaut
-        $dbPort="";
-        break;
-    default:
-        $dbHost="sgbd1.flsh.usherbrooke.ca";
-        # ligne suivante : le nom de votre base sql
-        $dbDb="grrmusique2016";
-        # ligne suivante : le nom de l'utilisateur sql qui a les droits sur la base
-        $dbUser="GRRmusique";
-        # ligne suivante : le mot de passe de l'utilisateur sql ci-dessus
-        $dbPass="music2013";
-        # ligne suivante : préfixe du nom des tables de données
-        $table_prefix="";
-        # ligne suivante : Port MySQL laissé par défaut
-        $dbPort="";
-}
+$dbHost="s";
+# ligne suivante : le nom de votre base sql
+$dbDb="";
+# ligne suivante : le nom de l'utilisateur sql qui a les droits sur la base
+$dbUser="";
+# ligne suivante : le mot de passe de l'utilisateur sql ci-dessus
+$dbPass="";
+# ligne suivante : préfixe du nom des tables de données
+$table_prefix="";
+# ligne suivante : Port MySQL laissé par défaut
+$dbPort="";
 ?>
