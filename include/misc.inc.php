cf4f8bc6020e9c71450f6659460436b11c9ca049
yn42 <yan.naessens@gmail.com>
2017-10-09 13:33:10 +0200

    ajout d'une table _calendrier_vacances, d'où modification du no de version et passage en RC

--- include/misc.inc.php
+++ include/misc.inc.php
@@ -34,12 +34,12 @@
 $grr_devel_email = "support@devome.com";
 $grr_devel_url = "http://grr.devome.com/";
 // Numéro de version actuel
-$version_grr = "3.3.0";
+$version_grr = "3.3.1";
 // Numéro de sous-version actuel (a, b, ...)
 // Utilisez cette variable pour des versions qui corrigent la la version finale sans toucher à la base.
-$sous_version_grr = "a"; // a, b, c, ...
+$sous_version_grr = ""; // a, b, c, ...
 // Numéro de la release candidate (doit être strictement inférieure à 9). Laisser vide s'il s'agit de la version stable.
-$version_grr_RC = "";
+$version_grr_RC = "1";
 
 # Liste des tables
 $liste_tables = array(
@@ -47,6 +47,7 @@ $liste_tables = array(
 	"_area_periodes",
 	"_calendar",
 	"_calendrier_jours_cycle",
+	"_calendrier_vacances",
 	"_entry",
 	"_entry_moderate",
 	"_type_area",
