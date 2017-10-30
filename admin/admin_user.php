9ebd1f57c23168d6f3636e83ab687d4d4dc263f7
yn42 <yan.naessens@gmail.com>
2017-10-30 11:10:57 +0100

    ajout d'un lien pour import d'utilisateurs depuis l'ENT elycée

--- admin/admin_user.php
+++ admin/admin_user.php
@@ -256,7 +256,8 @@ if (empty($order_by))
 ?>
 | <a href="admin_user_modify.php?display=<?php echo $display; ?>"><?php echo get_vocab("display_add_user"); ?></a> |
 <a href="admin_import_users_csv.php"><?php echo get_vocab("display_add_user_list_csv"); ?></a> |
-<a href="admin_user_mdp_facile.php"><?php echo get_vocab("admin_user_mdp_facile"); ?></a> |
+ <a href="admin_import_users_elycee.php">Importer des utilisateurs depuis elycée</a> |
+ <a href="admin_user_mdp_facile.php"><?php echo get_vocab("admin_user_mdp_facile"); ?></a> |
 <?php
 // On propose de supprimer les utilisateurs ext de GRR qui ne sont plus présents dans la base LCS
 if (Settings::get("sso_statut") == "lcs")
