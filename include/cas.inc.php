4beab656bf89bd904dd65801d32fc73c04f9b903
JeromeDevome <support@devome.com>
2017-12-16 14:36:30 +0100

    Modification Copyrigth - Include

--- include/cas.inc.php
+++ include/cas.inc.php
@@ -2,17 +2,10 @@
 /**
  * cas.inc.php
  * script de redirection vers l'authentification CAS
- * Ce script fait partie de l'application GRR
- * Dernière modification : $Date: 2010-04-07 15:38:14 $
- * @author    Laurent Delineau <laurent.delineau@ac-poitiers.fr>
- * @copyright Copyright 2008-2008 Laurent Delineau
- * @author    Olivier Mounier
- * @author    Laurent Delineau
- * @copyright Copyright 2005 Olivier Mounier
+ * Dernière modification : $Date: 2017-12-16 14:00$
+ * @author    JeromeB & Laurent Delineau & Olivier MOUNIER
+ * @copyright Copyright 2003-2018 Team DEVOME - JeromeB
  * @link      http://www.gnu.org/licenses/licenses.html
- * @package   root
- * @version   $Id: cas.inc.php,v 1.7 2010-04-07 15:38:14 grr Exp $
- * @filesource
  *
  * This file is part of GRR.
  *
@@ -20,16 +13,8 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- *
- * GRR is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with GRR; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
+
 // Le package phpCAS doit etre stocké dans un sous-répertoire « CAS »
 // dans un répertoire correspondant a l'include_path du php.ini (exemple : /var/lib/php)
 include_once(dirname(__FILE__).'/CAS/CAS.php');
--- include/generationxml.php
+++ include/generationxml.php
@@ -2,14 +2,10 @@
 /**
  * generationxml.php
  *
- * Ce script fait partie de l'application GRR
- * Dernière modification : $Date: 2009-01-20 07:19:17 $
+ * Dernière modification : $Date: 2017-12-16 14:00$
  * @author    JeromeB
- * @copyright Copyright 201-2015 JeromeB
+ * @copyright Copyright 2003-2018 Team DEVOME - JeromeB
  * @link      http://www.gnu.org/licenses/licenses.html
- * @package   root
- * @version   $Id: generationxml.php,v 1.3 2009-01-20 07:19:17 grr Exp $
- * @filesource
  *
  * This file is part of GRR.
  *
@@ -17,47 +13,8 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- *
- * GRR is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with GRR; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
- 
- /*
-if (@file_exists('../include/connect.inc.php')){
-	$racine = "../";
-}else{
-	$racine = "./";
-}
 
-	include $racine."include/connect.inc.php";
-	include $racine."include/config.inc.php";
-	include $racine."include/mrbs_sql.inc.php";
-	include $racine."include/misc.inc.php";
-	include $racine."include/functions.inc.php";
-	include $racine."include/$dbsys.inc.php";
-*//*
-// Settings
-require_once($racine."include/settings.class.php");
-//Chargement des valeurs de la table settingS
-if (!Settings::load())
-	die("Erreur chargement settings");
-// Session related functions
-require_once($racine."include/session.inc.php");
-// Resume session
-if (!grr_resumeSession()) {
-	header("Location: {$racine}logout.php?auto=1&url=$url");
-	die();
-};
-// Paramètres langage
-$use_admin = 'y';
-include $racine."include/language.inc.php";
-*/
 $temp = time();
 $result = grr_sql_query("SELECT * FROM ".TABLE_PREFIX."_entry WHERE end_time > '{$temp}';");
 
