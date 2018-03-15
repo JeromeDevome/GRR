9dc89011d74323e73bf39b0e615d4a243cba5e4e
yn42 <yan.naessens@gmail.com>
2018-03-15 17:09:18 +0100

    montée de sous-version en 3.3.1b

--- include/misc.inc.php
+++ include/misc.inc.php
@@ -25,7 +25,7 @@ $grr_devel_url = "http://grr.devome.com/";
 $version_grr = "3.3.1";
 // Numéro de sous-version actuel (a, b, ...)
 // Utilisez cette variable pour des versions qui corrigent la la version finale sans toucher à la base.
-$sous_version_grr = "a"; // a, b, c, ...
+$sous_version_grr = "b"; // a, b, c, ...
 // Numéro de la release candidate (doit être strictement inférieure à 9). Laisser vide s'il s'agit de la version stable.
 $version_grr_RC = "";
 
