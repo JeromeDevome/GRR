7eb365ae54aec454ecfb594fbaad073ceb208bbd
JeromeDevome <support@devome.com>
2018-08-05 21:01:28 +0200

    AdminLTE
    Nouveau theme pour l'administration

---
+++ bootstrap/AdminLTE/bower_components/ckeditor/config.js
@@ -0,0 +1,26 @@
+/**
+ * @license Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	config.extraPlugins = 'colorbutton';
+	config.toolbarGroups = [
+		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
+		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+		{ name: 'forms', groups: [ 'forms' ] },
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+		{ name: 'links', groups: [ 'links' ] },
+		{ name: 'insert', groups: [ 'insert' ] },
+		'/',
+		{ name: 'styles', groups: [ 'styles' ] },
+		{ name: 'colors', groups: [ 'colors' ] },
+		{ name: 'tools', groups: [ 'tools' ] },
+		{ name: 'others', groups: [ 'others' ] },
+		{ name: 'about', groups: [ 'about' ] }
+	];
+
+	config.removeButtons = 'Save,NewPage,Preview,Form,Checkbox,TextField,Textarea,Radio,Select,Button,ImageButton,HiddenField,CreateDiv,Language,BidiRtl,BidiLtr,Anchor,Flash,PageBreak,Iframe,ShowBlocks,About';
+};
