0fdb7bfcb765998bbae7c793c409d3eb1aeae5d9
yn42 <yan.naessens@gmail.com>
2018-08-23 17:28:52 +0200

    mise à jour en sous-version 3.4.0a

--- include/misc.inc.php
+++ include/misc.inc.php
@@ -2,7 +2,7 @@
 /**
  * misc.inc.php
  * fichier de variables diverses
- * Dernière modification : $Date: 2018-07-01 19:15$
+ * Dernière modification : $Date: 2018-08-23 18:15$
  * @author    JeromeB & Laurent Delineau
  * @copyright Copyright 2003-2018 Team DEVOME - JeromeB
  * @link      http://www.gnu.org/licenses/licenses.html
@@ -25,11 +25,11 @@ $grr_devel_url = "http://grr.devome.com/";
 $version_grr = "3.4.0";
 // Numéro de sous-version actuel (a, b, ...)
 // Utilisez cette variable pour des versions qui corrigent la la version finale sans toucher à la base.
-$sous_version_grr = ""; // a, b, c, ...
+$sous_version_grr = "a"; // a, b, c, ...
 // Numéro de la release candidate (doit être strictement inférieure à 9). Laisser vide s'il s'agit de la version stable.
 $version_grr_RC = "";
 // Version repository (GitHub)
-$versionReposite = "GitHub";
+$versionReposite = "";
 
 # Liste des tables
 $liste_tables = array(
