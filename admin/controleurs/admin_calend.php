1be566f37f493a667c53136c72445a0828dc78d4
JeromeDevome <support@devome.com>
2018-11-04 21:03:58 +0100

    Passage sous twig

---
+++ admin/controleurs/admin_calend.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * admin_calend.php
+ * interface permettant de choisir des outils de réservation en blocs
+ * Ce script fait partie de l'application GRR
+ * Dernière modification : $Date: 2017-12-16 14:00$
+ * @author    Laurent Delineau & JeromeB & Marc-Henri PAMISEUX & Yan Naessens
+ * @copyright Copyright 2003-2018 Team DEVOME - JeromeB
+ * @link      http://www.gnu.org/licenses/licenses.html
+ *
+ * This file is part of GRR.
+ *
+ * GRR is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+$grr_script_name = "admin_calend.php";
+
+// vérification des droits d'accès 
+if(authGetUserLevel(getUserName(),-1,'area') < 5)
+{
+    showAccessDenied($day, $month, $year, '',$back);
+    exit();
+}
+
+get_vocab_admin('admin_calendar_title');
+
+
+echo $twig->render('admin_calend.twig', array('liensMenu' => $menuAdminT, 'liensMenuN2' => $menuAdminTN2, 'trad' => $trad, 'settings' => $AllSettings));
+
+?>
