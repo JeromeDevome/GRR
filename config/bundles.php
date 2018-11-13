3de1957bc2ebe1fb4ac685255769ef6ca107db65
jfsenechal <jfsenechal@gmail.com>
2018-11-13 21:52:09 +0100

    Mise en place du framework symfony :
    composer create-project symfony/skeleton
    
    La commande va créer l'arborescence (bin, config, public, src...)
    
    composer require symfony/templating
    
    Pour utiliser les templates twig
    
    composer require maker
    
    Pour générer des controllers, entity,...
    
    composer require annotations
    
    Pour utiliser la puissance des annotations
    
    Créer un controller
    
    bin/console make:controller
    
    composer require symfony/twig-bundle
    
    pour dans un controller faire $this->render('nomtemplate');

---
+++ config/bundles.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+];
