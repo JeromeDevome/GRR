de6765e275d2e90f3ff2893639a30d930ad44b7a
Axel <axel.a.bertic@gmail.com>
2019-02-20 08:41:23 +0100

    [MAJ] Prise en charge de la room
    Prise en charge de la room afin de pouvoir utiliser le calendrier correctement.

--- calendar.php
+++ calendar.php
@@ -28,10 +28,15 @@
 							month = date.getMonth() + 1,              
 							year =  date.getFullYear();
 						var area = getQueryVariable("area");
+						var room = getQueryVariable("room");
 						var hostname = window.location.host;
 						var protocol = window.location.protocol;
-						self.location.replace(protocol +"//"+ hostname +"/day.php?area="+ area +"&day="+ day +"&year="+ year +"&month="+ month);
-						}  
+						if(room){
+							self.location.replace(protocol +"//"+ hostname +"/day.php?room="+ room +"&day="+ day +"&year="+ year +"&month="+ month);
+						} else {
+							self.location.replace(protocol +"//"+ hostname +"/day.php?area="+ area +"&day="+ day +"&year="+ year +"&month="+ month);
+						}
+					}  
                 } 
             );  
   
@@ -41,25 +46,29 @@
 				$( "#datepicker" ).datepicker({
 			});
 			
-			// Coloration des semaines au passage de la souris sur le numéro des semaines
+			// Highlight week on hover week number
 			$(document).on("mouseenter",".ui-datepicker-week-col",
 						   function(){$(this).siblings().find("a").addClass('ui-state-hover');} );
 			$(document).on("mouseleave",".ui-datepicker-week-col",
 						   function(){$(this).siblings().find("a").removeClass('ui-state-hover');} );
 			
-			// Selection de la semaine au clic sur le nombre d'une semaine, puis récupération de la première date
+			// Select week on click on week number
 			$(document).on("click",".ui-datepicker-week-col",
 			   function(){
 				   $first = $(this).siblings().find("a").first();
 				   $first.click();
 				   $parentFirst = $first.parent();
 				   var area = getQueryVariable("area");
+				   var room = getQueryVariable("room");
 				   var hostname = window.location.host;
 				   var protocol = window.location.protocol;
 				   var day  = $first.text(),  
 					   month = $parentFirst.data("month")+1,              
 					   year =  $parentFirst.data("year");
-				   self.location.replace(protocol +"//"+ hostname +"/week_all.php?area="+ area +"&day="+ day +"&year="+ year +"&month="+ month);
+				   if(room){
+						self.location.replace(protocol +"//"+ hostname +"/week.php?room="+ room +"&day="+ day +"&year="+ year +"&month="+ month);
+				   } else {
+						self.location.replace(protocol +"//"+ hostname +"/week_all.php?area="+ area +"&day="+ day +"&year="+ year +"&month="+ month);}
 					});
 				});
         });
