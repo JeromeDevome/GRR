363efec720f31f422ce075d733afbd8b0fb83b4e
yn42 <yan.naessens@gmail.com>
2019-10-10 11:47:40 +0200

    correction d'erreurs php

--- phpmailer/PHPMailerAutoload.php
+++ phpmailer/PHPMailerAutoload.php
@@ -38,12 +38,15 @@ if (version_compare(PHP_VERSION, '5.1.2', '>=')) {
         spl_autoload_register('PHPMailerAutoload');
     }
 } else {
+	echo 'version de php obsolète';
     /**
      * Fall back to traditional autoload for old PHP versions
      * @param string $classname The name of the class to load
      */
-    function __autoload($classname)
+    /*function __autoload($classname)
     {
         PHPMailerAutoload($classname);
     }
+	modification YN le 10/10/2019
+	*/
 }
