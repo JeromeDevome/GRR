c813ec1ce61d404e363e75cce9f1b6ddc189b241
JeromeB <support@devome.com>
2021-10-03 17:01:27 +0200

    Upgrade composants
      - Upgrading phpmailer/phpmailer (v6.5.0 => v6.5.1)
      - Upgrading symfony/polyfill-mbstring (v1.23.0 => v1.23.1)
      - Upgrading tecnickcom/tcpdf (6.4.1 => 6.4.2)
      - Upgrading twig/twig (v2.14.6 => v2.14.7)

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -31,12 +31,12 @@
     ),
     'phpmailer/phpmailer' => 
     array (
-      'pretty_version' => 'v6.5.0',
-      'version' => '6.5.0.0',
+      'pretty_version' => 'v6.5.1',
+      'version' => '6.5.1.0',
       'aliases' => 
       array (
       ),
-      'reference' => 'a5b5c43e50b7fba655f793ad27303cd74c57363c',
+      'reference' => 'dd803df5ad7492e1b40637f7ebd258fee5ca7355',
     ),
     'psr/log' => 
     array (
@@ -58,30 +58,30 @@
     ),
     'symfony/polyfill-mbstring' => 
     array (
-      'pretty_version' => 'v1.23.0',
-      'version' => '1.23.0.0',
+      'pretty_version' => 'v1.23.1',
+      'version' => '1.23.1.0',
       'aliases' => 
       array (
       ),
-      'reference' => '2df51500adbaebdc4c38dea4c89a2e131c45c8a1',
+      'reference' => '9174a3d80210dca8daa7f31fec659150bbeabfc6',
     ),
     'tecnickcom/tcpdf' => 
     array (
-      'pretty_version' => '6.4.1',
-      'version' => '6.4.1.0',
+      'pretty_version' => '6.4.2',
+      'version' => '6.4.2.0',
       'aliases' => 
       array (
       ),
-      'reference' => '5ba838befdb37ef06a16d9f716f35eb03cb1b329',
+      'reference' => '172540dcbfdf8dc983bc2fe78feff48ff7ec1c76',
     ),
     'twig/twig' => 
     array (
-      'pretty_version' => 'v2.14.6',
-      'version' => '2.14.6.0',
+      'pretty_version' => 'v2.14.7',
+      'version' => '2.14.7.0',
       'aliases' => 
       array (
       ),
-      'reference' => '27e5cf2b05e3744accf39d4c68a3235d9966d260',
+      'reference' => '8e202327ee1ed863629de9b18a5ec70ac614d88f',
     ),
   ),
 );
