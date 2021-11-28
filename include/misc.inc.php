839274a2472e6d9027958e2dd4b6d7003d1a3bd5
yn42 <yan.naessens@gmail.com>
2021-11-28 18:46:38 +0100

    version candidate 3.4.3 RC1

--- include/misc.inc.php
+++ include/misc.inc.php
@@ -2,9 +2,9 @@
 /**
  * misc.inc.php
  * fichier de variables diverses
- * Dernière modification : $Date: 2021-07-31 18:07$
+ * Dernière modification : $Date: 2021-11-28 18:37$
  * @author    JeromeB & Laurent Delineau & Yan Naessens
- * @copyright Copyright 2003-2020 Team DEVOME - JeromeB
+ * @copyright Copyright 2003-2021 Team DEVOME - JeromeB
  * @link      http://www.gnu.org/licenses/licenses.html
  *
  * This file is part of GRR.
@@ -27,7 +27,7 @@ $version_grr = "3.4.3";
 // Utilisez cette variable pour des versions qui corrigent la la version finale sans toucher à la base.
 $sous_version_grr = ""; // a, b, c, ...
 // Numéro de la release candidate (doit être strictement inférieure à 9). Laisser vide s'il s'agit de la version stable.
-$version_grr_RC = "0";
+$version_grr_RC = "1";
 // Version repository (GitHub)
 $versionReposite = "";
 
