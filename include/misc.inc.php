0cbe3752f601333111c96d0a0fffda704490f81a
yn42 <yan.naessens@gmail.com>
2023-04-03 09:59:51 +0200

    Version stable 3.5.0d

--- include/misc.inc.php
+++ include/misc.inc.php
@@ -2,7 +2,7 @@
 /**
  * misc.inc.php
  * fichier de variables diverses
- * Dernière modification : $Date: 2023-02-25 19:02$
+ * Dernière modification : $Date: 2023-04-03 09:52$
  * @author    JeromeB & Laurent Delineau & Yan Naessens
  * @copyright Copyright 2003-2023 Team DEVOME - JeromeB
  * @link      http://www.gnu.org/licenses/licenses.html
@@ -25,7 +25,7 @@ $grr_devel_url = "http://grr.devome.com/";
 $version_grr = "3.5.0";
 // Numéro de sous-version actuel (a, b, ...)
 // Utilisez cette variable pour des versions qui corrigent la la version finale sans toucher à la base.
-$sous_version_grr = "c"; // a, b, c, ...
+$sous_version_grr = "d"; // a, b, c, ...
 // Numéro de la release candidate (doit être strictement inférieure à 9). Laisser vide s'il s'agit de la version stable.
 $version_grr_RC = "";
 // Version repository (GitHub)
