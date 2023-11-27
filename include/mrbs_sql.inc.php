dc6d43d95324f271c2f70346ebb9f7bc4916818a
JeromeB <commercial@devome.com>
2023-11-27 21:26:05 +0100

    [FIX] Email spec, champ additionnel

--- include/mrbs_sql.inc.php
+++ include/mrbs_sql.inc.php
@@ -371,6 +371,8 @@ function mrbsEntryGetOverloadDesc($id_entry)
 			$overload_array[$field]["overload_mail"] = grr_sql_query1("SELECT overload_mail FROM ".TABLE_PREFIX."_overload WHERE id = '".$fieldslist[$field]["id"]."'");
 			$overload_array[$field]["obligatoire"] = grr_sql_query1("SELECT obligatoire FROM ".TABLE_PREFIX."_overload WHERE id = '".$fieldslist[$field]["id"]."'");
 			$overload_array[$field]["confidentiel"] = grr_sql_query1("SELECT confidentiel FROM ".TABLE_PREFIX."_overload WHERE id = '".$fieldslist[$field]["id"]."'");
+			$overload_array[$field]["mail_spec"] = grr_sql_query1("SELECT mail_spec FROM ".TABLE_PREFIX."_overload WHERE id = '".$fieldslist[$field]["id"]."'");
+
 		}
 		return $overload_array;
 	}
