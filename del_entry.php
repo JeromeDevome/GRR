6e26e0f50443fca8257a60a60b7b6da674597f22
yn42 <yan.naessens@gmail.com>
2023-11-28 18:55:48 +0100

    amélioration : typage des valeurs

--- del_entry.php
+++ del_entry.php
@@ -3,9 +3,9 @@
  * del_entry.php
  * Interface de suppression d'une réservation
  * Ce script fait partie de l'application GRR
- * Dernière modification : $Date: 2022-06-18 11:59$
+ * Dernière modification : $Date: 2023-11-22 16:22$
  * @author    Laurent Delineau & JeromeB & Yan Naessens
- * @copyright Copyright 2003-2022 Team DEVOME - JeromeB
+ * @copyright Copyright 2003-2023 Team DEVOME - JeromeB
  * @link      http://www.gnu.org/licenses/licenses.html
  *
  * This file is part of GRR.
@@ -16,12 +16,13 @@
  * (at your option) any later version.
  */
 $grr_script_name = "del_entry.php";
+
 include "include/connect.inc.php";
 include "include/config.inc.php";
-include "include/functions.inc.php";
+include "include/misc.inc.php";
 include "include/$dbsys.inc.php";
-include_once('include/misc.inc.php');
 include "include/mrbs_sql.inc.php";
+include "include/functions.inc.php";
 
 require_once("./include/settings.class.php");
 if (!Settings::load())
@@ -32,17 +33,13 @@ if (!grr_resumeSession())
 	header("Location: ./logout.php?auto=1&url=$url");
 	die();
 };
+$user_name = getUserName();
 include "include/language.inc.php";
-$series = isset($_GET["series"]) ? $_GET["series"] : NULL;
-if (isset($series))
-	settype($series,"integer");
+$series = getFormVar("series","integer");
 $page = verif_page();
-if (isset($_GET["id"]))
-{
-	$id = clean_input($_GET["id"]);
-	settype($id,"integer");
-}
-else{
+$id = getFormVar("id","integer");
+$id = clean_input($id);
+if(is_null($id)){
 	header("Location: ./login.php");
 	die();
 }
@@ -51,20 +48,20 @@ if ($info = mrbsGetEntryInfo($id))
 	$day   = date("d", $info["start_time"]);
 	$month = date("m", $info["start_time"]);
 	$year  = date("Y", $info["start_time"]);
-	$area  = mrbsGetRoomArea($info["room_id"]);
+    $room_id = intval($info["room_id"]);
+	$area  = mrbsGetRoomArea($room_id);
 	$back = (isset($_SERVER['HTTP_REFERER']))? htmlspecialchars_decode($_SERVER['HTTP_REFERER'], ENT_QUOTES) : page_accueil() ;
-	if (authGetUserLevel(getUserName(), -1) < 1)
+	if (authGetUserLevel($user_name, -1) < 1)
 	{
 		showAccessDenied($back);
 		exit();
 	}
-	//if (!getWritable($info["beneficiaire"], getUserName(), $id))
-    if (!getWritable(getUserName(), $id))
+    if (!getWritable($user_name, $id))
 	{
 		showAccessDenied($back);
 		exit;
 	}
-	if (authUserAccesArea(getUserName(), $area) == 0)
+	if (authUserAccesArea($user_name, $area) == 0)
 	{
 		showAccessDenied($back);
 		exit();
@@ -76,17 +73,16 @@ if ($info = mrbsGetEntryInfo($id))
         $_SESSION['session_message_error'] .= send_mail($id,3,$dformat);
     }
     display_mail_msg();
-	$room_id = grr_sql_query1("SELECT ".TABLE_PREFIX."_entry.room_id FROM ".TABLE_PREFIX."_entry WHERE ".TABLE_PREFIX."_entry.id='".$id."'");
 	$date_now = time();
 	get_planning_area_values($area);
     $who_can_book = grr_sql_query1("SELECT who_can_book FROM ".TABLE_PREFIX."_room WHERE id='".$room_id."' ");
-    $user_can_book = $who_can_book || (authBooking($current_user,$info_alt['room_id']));
-	if ((!(verif_booking_date(getUserName(), $id, $room_id, -1, $date_now, $enable_periods))) || ((verif_booking_date(getUserName(), $id, $room_id, -1, $date_now, $enable_periods)) && ($can_delete_or_create != "y")) && $user_can_book)
+    $user_can_book = $who_can_book || (authBooking($current_user,$room_id));
+	if ((!(verif_booking_date($user_name, $id, $room_id, -1, $date_now, $enable_periods))) || ((verif_booking_date($user_name, $id, $room_id, -1, $date_now, $enable_periods)) && ($can_delete_or_create != "y")) && $user_can_book)
 	{
 		showAccessDenied($back);
 		exit();
 	}
-	$result = mrbsDelEntry(getUserName(), $id, $series, 1);
+	$result = mrbsDelEntry($user_name, $id, $series, 1);
 	if ($result)
 	{
         $room_back = isset($_GET['room_back']) ? clean_input($_GET['room_back']) : $info['room_id'];
