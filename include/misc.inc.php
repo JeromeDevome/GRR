f795035288e3faaa30f3e50e0202278db481c29b
JeromeB <commercial@devome.com>
2024-06-12 19:52:05 +0200

    Update misc.inc.php

--- include/misc.inc.php
+++ include/misc.inc.php
@@ -23,12 +23,12 @@ $grr_devel_url = "https://grr.devome.com/";
 
 // Numéro de version actuel
 # Format X.X.XRCX | exemples : 4.0.0a (alpha) ou 4.0.0b (beta) 3.4.0RC1 (Release Candidate) ou 3.4.0 (version OK)
-$version_grr = "4.3.4";
+$version_grr = "4.3.5";
 # Version BDD, deux premirs chiffres = version majeur, les deux suivant la version, évolution de GRR, les 3 derniers une incrémentation à chaque changement
 # Ex 0400003 : 3eme modification sur la branche 4.X.X
 $version_bdd = "0400004";
 // Version repository (GitHub) GitHub-Master / Release-v4.0.0-beta.1 / Release-v4.0.0-RC.1 / Release-v4.0.0
-$versionReposite = "GitHub-Branch-4.3.X";
+$versionReposite = "Release-v4.3.5";
 
 ################################
 # Configuration Requise
@@ -36,7 +36,7 @@ $versionReposite = "GitHub-Branch-4.3.X";
 // Version PHP minimum
 $php_mini = "7.2.5";
 // Version PHP maximum testé et validé par : JeromeB
-$php_max_valide = "8.3.2";
+$php_max_valide = "8.3.8";
 // Version PHP maximum qui est sensé fonctionné, si compatible avec toutes les versions à ce jour laisser vide
 $php_maxi = "";
 
