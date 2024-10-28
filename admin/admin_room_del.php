ccf0ed361f5052b2ccc4bd7b0ea012fa5c4c299c
Yan <yan.naessens@gmail.com>
2024-10-28 19:51:46 +0100

    amélioration : utilisation de requêtes préparées, reprise du code

--- admin/admin_room_del.php
+++ admin/admin_room_del.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * admin_room_del
- * Interface de confirmation de suppression d'un domaine ou d'une ressource
+ * Interface de suppression d'un domaine ou d'une ressource : confirmation puis suppression
  * Ce script fait partie de l'application GRR
- * Dernière modification : $Date: 2021-03-13 11:41$
+ * Dernière modification : $Date: 2024-10-28 16:23$
  * @author    Laurent Delineau & JeromeB & Yan Naessens
- * @copyright Copyright 2003-2021 Team DEVOME - JeromeB
+ * @copyright Copyright 2003-2024 Team DEVOME - JeromeB
  * @link      http://www.gnu.org/licenses/licenses.html
  *
  * This file is part of GRR.
@@ -21,19 +21,18 @@ include "../include/admin.inc.php";
 
 $type = isset($_GET["type"]) ? $_GET["type"] : NULL;
 $confirm = isset($_GET["confirm"]) ? $_GET["confirm"] : NULL;
-$room = isset($_GET["room"]) ? $_GET["room"] : NULL;
-$id_area = isset($_POST["id_area"]) ? $_POST["id_area"] : (isset($_GET["id_area"]) ? $_GET["id_area"] : NULL);
+$room = isset($_GET["room"]) ? $_GET["room"] : -1;
+$id_area = isset($_POST["id_area"]) ? $_POST["id_area"] : (isset($_GET["id_area"]) ? $_GET["id_area"] : -1);
 $id_site = isset($_POST['id_site']) ? $_POST['id_site'] : (isset($_GET['id_site']) ? $_GET['id_site'] : -1);
-if (isset($room))
-	settype($room,"integer");
-if (isset($id_area))
-	settype($id_area,"integer");
-if (isset($id_site))
-	settype($id_site,"integer");
+$room = intval($room);
+$id_area = intval($id_area);
+$id_site = intval($id_site);
+$user_id = getUserName();
 $back = (isset($_SERVER['HTTP_REFERER']))? htmlspecialchars_decode($_SERVER['HTTP_REFERER'], ENT_QUOTES) : "./admin_accueil.php" ;
+
 if ($type == "room")
 {
-	if ((authGetUserLevel(getUserName(),$room) < 4) || (!verif_acces_ressource(getUserName(), $room)))
+	if ((authGetUserLevel($user_id,$room) < 4) || (!verif_acces_ressource($user_id, $room)))
 	{
 		showAccessDenied($back);
 		exit();
@@ -42,99 +41,114 @@ if ($type == "room")
 	{
 		//They have confirmed it already, so go blast!
 		//First take out all appointments for this room
-		grr_sql_command("DELETE FROM ".TABLE_PREFIX."_entry WHERE room_id=$room");
-		grr_sql_command("DELETE FROM ".TABLE_PREFIX."_entry_moderate WHERE room_id=$room");
-		grr_sql_command("DELETE FROM ".TABLE_PREFIX."_j_mailuser_room  WHERE id_room=$room");
-		grr_sql_command("DELETE FROM ".TABLE_PREFIX."_j_user_room WHERE id_room=$room");
-        grr_sql_command("DELETE FROM ".TABLE_PREFIX."_j_userbook_room WHERE id_room=$room");
+		grr_sql_command("DELETE FROM ".TABLE_PREFIX."_entry WHERE room_id=?","i",[$room]);
+		grr_sql_command("DELETE FROM ".TABLE_PREFIX."_entry_moderate WHERE room_id=?","i",[$room]);
+		grr_sql_command("DELETE FROM ".TABLE_PREFIX."_j_mailuser_room  WHERE id_room=?","i",[$room]);
+		grr_sql_command("DELETE FROM ".TABLE_PREFIX."_j_user_room WHERE id_room=?","i",[$room]);
+        grr_sql_command("DELETE FROM ".TABLE_PREFIX."_j_userbook_room WHERE id_room=?","i",[$room]);
 		//Now take out the room itself
-		grr_sql_command("DELETE FROM ".TABLE_PREFIX."_room WHERE id=$room");
+		grr_sql_command("DELETE FROM ".TABLE_PREFIX."_room WHERE id=?","i",[$room]);
 		//Go back to the admin page
 		Header("Location: admin_room.php?id_area=$id_area&id_site=$id_site");
 	}
 	else
 	{
-		//print the page header
-		start_page_w_header("", "", "", $type="with_session");
-		echo "<div class='container'>";
-		//We tell them how bad what theyre about to do is
+    $cas = 0;
+    $resas = array();
 		//Find out how many appointments would be deleted
-		$sql = "SELECT name, start_time, end_time FROM ".TABLE_PREFIX."_entry WHERE room_id=$room";
-		$res = grr_sql_query($sql);
+		$sql = "SELECT name, start_time, end_time FROM ".TABLE_PREFIX."_entry WHERE room_id=?";
+		$res = grr_sql_query($sql,"i",[$room]);
 		if (!$res)
-			echo grr_sql_error();
+			fatal_error(1,grr_sql_error());
 		else if (grr_sql_count($res) > 0)
 		{
-			echo "<p class='avertissement larger'>".get_vocab("deletefollowing")." :</p><ul>";
-			for ($i = 0; ($row = grr_sql_row($res, $i)); $i++)
+			foreach($res as $row)
 			{
-				echo "<li>$row[0] (";
-					echo time_date_string($row[1],$dformat) . " -> ";
-					echo time_date_string($row[2],$dformat) . ")";
+        $resas[] = $row;
 			}
-			echo "</ul>";
 		}
-		echo "<h1 class='center'>" . get_vocab("sure") . "</h1>";
-		echo "<h1 class='center'><a href=\"admin_room_del.php?type=room&amp;room=$room&amp;confirm=Y&amp;id_area=$id_area\">" . get_vocab("YES") . "!</a>     <a href=\"admin_room.php?id_area=$id_area\">" . get_vocab("NO") . "!</a></h1>";
+
 		echo "</div>";
 	}
 }
 if ($type == "area")
 {
 	// Seul l'admin peut supprimer un domaine
-	if (authGetUserLevel(getUserName(), $id_area, 'area') < 5)
+	if (authGetUserLevel($user_id, $id_area, 'area') < 5)
 	{
 		showAccessDenied($back);
 		exit();
 	}
 	//We are only going to let them delete an area if there are
 	//no rooms. its easier
-	$n = grr_sql_query1("SELECT count(*) FROM ".TABLE_PREFIX."_room WHERE area_id=$id_area");
+	$n = grr_sql_query1("SELECT count(*) FROM ".TABLE_PREFIX."_room WHERE area_id=?","i",[$id_area]);
+  echo $n;
 	if ($n == 0)
 	{
 		// Suppression des champ additionnels
-		$sqlstring = "SELECT id FROM ".TABLE_PREFIX."_overload WHERE id_area='".$id_area."'";
-		$result = grr_sql_query($sqlstring);
-		for ($i = 0; ($field_row = grr_sql_row($result, $i)); $i++)
-		{
-			$id_overload = $field_row[0];
-			// Suppression des données dans les réservations déjà effectuées
-			grrDelOverloadFromEntries($id_overload);
-			$sql = "DELETE FROM ".TABLE_PREFIX."_overload WHERE id=$id_overload;";
-			grr_sql_command($sql);
-		}
-		//OK, nothing there, lets blast it away
-		grr_sql_command("DELETE FROM ".TABLE_PREFIX."_area WHERE id=$id_area");
-		grr_sql_command("update ".TABLE_PREFIX."_utilisateurs set default_area = '-1', default_room = '-1' WHERE default_area='".$id_area."'");
-		grr_sql_command("DELETE FROM ".TABLE_PREFIX."_area_periodes WHERE id_area=$id_area");
-		grr_sql_command("DELETE FROM ".TABLE_PREFIX."_j_useradmin_area WHERE id_area=$id_area");
-		grr_sql_command("DELETE FROM ".TABLE_PREFIX."_j_type_area WHERE id_area=$id_area");
-		grr_sql_command("DELETE FROM ".TABLE_PREFIX."_j_user_area WHERE id_area=$id_area");
-		grr_sql_command("DELETE FROM ".TABLE_PREFIX."_j_site_area WHERE id_area=$id_area");
-		$test = grr_sql_query1("select VALUE from ".TABLE_PREFIX."_setting WHERE NAME='default_area'");
-		if ($test == $id_area)
-		{
-			grr_sql_command("DELETE FROM ".TABLE_PREFIX."_setting WHERE NAME='default_area'");
-			grr_sql_command("DELETE FROM ".TABLE_PREFIX."_setting WHERE NAME='default_room'");
-			// Settings
-			require_once("../include/settings.class.php");
-			//Chargement des valeurs de la table settingS
-			if (!Settings::load())
-				die("Erreur chargement settings");
-		}
-		//Redirect back to the admin page
-		header("Location: admin_room.php?id_site=$id_site");
-        die();
+		$sqlstring = "SELECT id FROM ".TABLE_PREFIX."_overload WHERE id_area=?";
+		$result = grr_sql_query($sqlstring,"i",[$id_area]);
+    if(!$result)
+      fatal_error(1,grr_sql_error());
+    else{
+      foreach($result as $field_row){
+        $id_overload = $field_row['id'];
+        // Suppression des données dans les réservations déjà effectuées
+        grrDelOverloadFromEntries($id_overload);
+        grr_sql_command("DELETE FROM ".TABLE_PREFIX."_overload WHERE id=?","i",[$id_overload]);
+      }
+      //OK, nothing there, lets blast it away
+      grr_sql_command("DELETE FROM ".TABLE_PREFIX."_area WHERE id=?","i",[$id_area]);
+      grr_sql_command("update ".TABLE_PREFIX."_utilisateurs set default_area = '-1', default_room = '-1' WHERE default_area=?","i",[$id_area]);
+      grr_sql_command("DELETE FROM ".TABLE_PREFIX."_area_periodes WHERE id_area=?","i",[$id_area]);
+      grr_sql_command("DELETE FROM ".TABLE_PREFIX."_j_useradmin_area WHERE id_area=?","i",[$id_area]);
+      grr_sql_command("DELETE FROM ".TABLE_PREFIX."_j_type_area WHERE id_area=?","i",[$id_area]);
+      grr_sql_command("DELETE FROM ".TABLE_PREFIX."_j_user_area WHERE id_area=?","i",[$id_area]);
+      grr_sql_command("DELETE FROM ".TABLE_PREFIX."_j_site_area WHERE id_area=?","i",[$id_area]);
+      $test = grr_sql_query1("select VALUE from ".TABLE_PREFIX."_setting WHERE NAME='default_area'");
+      if ($test == $id_area)
+      {
+        grr_sql_command("DELETE FROM ".TABLE_PREFIX."_setting WHERE NAME='default_area'");
+        grr_sql_command("DELETE FROM ".TABLE_PREFIX."_setting WHERE NAME='default_room'");
+        // Settings
+        require_once("../include/settings.class.php");
+        //Chargement des valeurs de la table settingS
+        if (!Settings::load())
+          die("Erreur chargement settings");
+      }
+      //Redirect back to the admin page
+      header("Location: admin_room.php?id_site=$id_site");
+      die();
+    }
 	}
 	else
-	{
-		//There are rooms left in the area
-		//print the page header
-		start_page_w_header("", "", "", $type="with_session");
-		echo "<div class=\"container\">";
-		echo "<p class='avertissement larger'>".get_vocab('delarea')."</p>";
-		echo "<p><a href=\"admin_room.php?id_area=$id_area&amp;id_site=$id_site\">" . get_vocab('back') . "</a></p></div>";
+	{//There are rooms left in the area
+		$cas = 1;
 	}
 }
+// html
+start_page_w_header("", "", "", $type="with_session");
+echo "<div class=\"container\">";
+if($cas == 1){
+  echo "<p class='avertissement larger'>".get_vocab('delarea')."</p>";
+  echo "<p><a class='btn btn-default' href=\"admin_room.php?id_area=$id_area&amp;id_site=$id_site\">" . get_vocab('back') . "</a></p></div>";
+}
+if($cas == 0){
+  // demande confirmation car les réservations vont être supprimées
+  if(count($resas) > 0){
+    echo "<p class='avertissement larger'>".get_vocab("deletefollowing")." :</p>";
+    echo "<ul>";
+    foreach($resas as $row){
+    	echo "<li>".$row['name']." (".time_date_string($row['start_time'],$dformat)." -> ".time_date_string($row['end_time'],$dformat) . ")</li>";
+    }
+    echo "</ul>";
+  }
+  echo "<h1 class='center'>" . get_vocab("sure") . "</h1>";
+  echo "<p class='center larger'>";
+  echo "<a class=\"btn btn-danger\" type=\"button\" href=\"admin_room_del.php?type=room&amp;room=$room&amp;confirm=Y&amp;id_area=$id_area\">".get_vocab("YES")."!</a>";
+  echo "&nbsp;&nbsp;<a class=\"btn btn-default\" type=\"button\" href=\"admin_room.php?id_area=$id_area\">".get_vocab("NO") . "!</a>";
+  echo "</p>";
+}
+echo "</div>";
 end_page();
 ?>
