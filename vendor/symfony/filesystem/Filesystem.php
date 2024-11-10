3bd2d0707b290da264e9c2acbda02e8c8a69cfc4
JeromeB <commercial@devome.com>
2024-11-10 17:19:06 +0100

    Mise à jour des librairies PHP

--- vendor/symfony/filesystem/Filesystem.php
+++ vendor/symfony/filesystem/Filesystem.php
@@ -44,7 +44,7 @@ class Filesystem
         $this->mkdir(\dirname($targetFile));
 
         $doCopy = true;
-        if (!$overwriteNewerFiles && null === parse_url($originFile, \PHP_URL_HOST) && is_file($targetFile)) {
+        if (!$overwriteNewerFiles && !parse_url($originFile, \PHP_URL_HOST) && is_file($targetFile)) {
             $doCopy = filemtime($originFile) > filemtime($targetFile);
         }
 
@@ -234,6 +234,7 @@ class Filesystem
      * Change the owner of an array of files or directories.
      *
      * This method always throws on Windows, as the underlying PHP function is not supported.
+     *
      * @see https://www.php.net/chown
      *
      * @param string|iterable $files     A filename, an array of files, or a \Traversable instance to change owner
@@ -264,6 +265,7 @@ class Filesystem
      * Change the group of an array of files or directories.
      *
      * This method always throws on Windows, as the underlying PHP function is not supported.
+     *
      * @see https://www.php.net/chgrp
      *
      * @param string|iterable $files     A filename, an array of files, or a \Traversable instance to change group
--- vendor/symfony/http-kernel/Kernel.php
+++ vendor/symfony/http-kernel/Kernel.php
@@ -78,15 +78,15 @@ abstract class Kernel implements KernelInterface, RebootableInterface, Terminabl
      */
     private static $freshCache = [];
 
-    public const VERSION = '5.4.44';
-    public const VERSION_ID = 50444;
+    public const VERSION = '5.4.46';
+    public const VERSION_ID = 50446;
     public const MAJOR_VERSION = 5;
     public const MINOR_VERSION = 4;
-    public const RELEASE_VERSION = 44;
+    public const RELEASE_VERSION = 46;
     public const EXTRA_VERSION = '';
 
     public const END_OF_MAINTENANCE = '11/2024';
-    public const END_OF_LIFE = '11/2025';
+    public const END_OF_LIFE = '02/2029';
 
     public function __construct(string $environment, bool $debug)
     {
