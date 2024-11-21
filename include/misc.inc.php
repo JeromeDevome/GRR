9347e05ec8bdf7cf7803533124aa0662f231056f
Yan <ynaessens@users.noreply.github.com>
2024-11-21 09:49:35 +0100

    Version candidate 3.5.2 RC1

--- include/misc.inc.php
+++ include/misc.inc.php
@@ -2,9 +2,9 @@
 /**
  * misc.inc.php
  * fichier de variables diverses
- * Dernière modification : $Date: 2023-04-22 11:47$
+ * Dernière modification : $Date: 2024-11-21 09:45$
  * @author    JeromeB & Laurent Delineau & Yan Naessens
- * @copyright Copyright 2003-2023 Team DEVOME - JeromeB
+ * @copyright Copyright 2003-2024 Team DEVOME - JeromeB
  * @link      http://www.gnu.org/licenses/licenses.html
  *
  * This file is part of GRR.
@@ -27,7 +27,7 @@ $version_grr = "3.5.2";
 // Utilisez cette variable pour des versions qui corrigent la la version finale sans toucher à la base.
 $sous_version_grr = ""; // a, b, c, ...
 // Numéro de la release candidate (doit être strictement inférieure à 9). Laisser vide s'il s'agit de la version stable.
-$version_grr_RC = "0";
+$version_grr_RC = "1";
 // Version repository (GitHub)
 $versionReposite = "";
 
