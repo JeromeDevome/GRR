b37217f0131eb1e413ebf4fd8b4b60369a17a285
JeromeB <commercial@devome.com>
2025-07-06 10:44:01 +0200

    Correction UTF8
    Co-Authored-By: Rico <91775199+bambi1013@users.noreply.github.com>
    
    #449

--- include/mail.class.php
+++ include/mail.class.php
@@ -87,8 +87,8 @@ class Email{
 				}
 
 			} elseif (Settings::get('grr_mail_method') == 'mail') {
-        $headers  = 'MIME-Version: 1.0' . "\r\n";
-        $headers .= 'Content-type: text/html; charset=UTF8' . "\r\n";
+       			$headers  = 'MIME-Version: 1.0' . "\r\n";
+      			$headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
 				$headers .= "From: {$DE}" . "\r\n" .
 					"Reply-To: {$DE}" . "\r\n" .
 					'X-Mailer: PHP/' . phpversion();
