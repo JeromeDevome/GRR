807b2507ced05e261d686c14278cd261bfc36226
Yan <ynaessens@users.noreply.github.com>
2025-07-07 18:06:15 +0200

    correctif : évite une erreur en mise à jour forcée

--- admin/admin_maj.php
+++ admin/admin_maj.php
@@ -993,11 +993,11 @@ if (isset($_POST['maj']) || isset($_GET['force_maj']) || $majscript)
     if($version_old < "3.5.2.9")
     {
         $result .= formatResult("Mise à jour jusqu'à la version 3.5.2 :","<b>","</b>");
-        $result_inter .= traiteRequete("ALTER TABLE `".TABLE_PREFIX."_room` ADD `max_booking_on_range` SMALLINT(6) NOT NULL DEFAULT '-1' AFTER `who_can_book`;");
+        $result_inter .= traiteRequete("ALTER TABLE `".TABLE_PREFIX."_room` ADD IF NOT EXISTS `max_booking_on_range` SMALLINT(6) NOT NULL DEFAULT '-1' AFTER `who_can_book`;");
         $result_inter .= traiteRequete("CREATE TABLE IF NOT EXISTS ".TABLE_PREFIX."_files(id int not null auto_increment, id_entry int, file_name varchar(50), public_name varchar(50),Primary key (id)) CHARACTER SET utf8mb4;");
-        $result_inter .= traiteRequete("ALTER TABLE `".TABLE_PREFIX."_area` ADD `user_right` INT(11) AFTER `max_booking`;");
-        $result_inter .= traiteRequete("ALTER TABLE `".TABLE_PREFIX."_area` ADD `access_file` INT(11) AFTER `user_right`;");
-        $result_inter .= traiteRequete("ALTER TABLE `".TABLE_PREFIX."_area` ADD `upload_file` INT(11) AFTER `access_file`;");
+        $result_inter .= traiteRequete("ALTER TABLE `".TABLE_PREFIX."_area` ADD IF NOT EXISTS `user_right` INT(11) AFTER `max_booking`;");
+        $result_inter .= traiteRequete("ALTER TABLE `".TABLE_PREFIX."_area` ADD IF NOT EXISTS `access_file` INT(11) AFTER `user_right`;");
+        $result_inter .= traiteRequete("ALTER TABLE `".TABLE_PREFIX."_area` ADD IF NOT EXISTS `upload_file` INT(11) AFTER `access_file`;");
         if ($result_inter == '')
             $result .= formatResult("Ok !","<span style='color:green;'>","</span>");
         else
