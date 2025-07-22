aa00eec44cb9500e988b9c8843de7d2d7af75992
JeromeB <commercial@devome.com>
2025-07-22 18:57:32 +0200

    Update misc.inc.php

--- include/misc.inc.php
+++ include/misc.inc.php
@@ -28,7 +28,7 @@ $version_grr = "4.4.1";
 # Ex 0400003 : 3eme modification sur la branche 4.X.X
 $version_bdd = "0400007";
 // Version repository (GitHub) GitHub-Master / Release-v4.0.0-beta.1 / Release-v4.0.0-RC.1 / Release-v4.0.0
-$versionReposite = "Release-v4.4.1";
+$versionReposite = "Release-v4.4.1-GitHub-Master";
 
 ################################
 # Configuration Requise
