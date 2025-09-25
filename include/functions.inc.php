e159069e068ec5161ce28af4f58e85b78377d076
DavidDelehelle <david.delehelle@laposte.net>
2025-09-25 09:26:17 +0200

    select distinct for dumplicate key on synchro group

--- include/functions.inc.php
+++ include/functions.inc.php
@@ -2006,7 +2006,7 @@ function synchro_groupe($idGroupe, $action)
 		{
 			for ($i = 0; ($row2 = grr_sql_row($res2, $i)); $i++)
 			{
-				$sql3 = "SELECT ug.login FROM ".TABLE_PREFIX."_j_group_site jg LEFT JOIN ".TABLE_PREFIX."_utilisateurs_groupes ug ON jg.idgroupes = ug.idgroupes WHERE jg.id_site = $row2[0] AND jg.idgroupes = $idGroupe";
+				$sql3 = "SELECT DISTINCT ug.login FROM ".TABLE_PREFIX."_j_group_site jg LEFT JOIN ".TABLE_PREFIX."_utilisateurs_groupes ug ON jg.idgroupes = ug.idgroupes WHERE jg.id_site = $row2[0] AND jg.idgroupes = $idGroupe";
 				$res3 = grr_sql_query($sql3);
 
 				for ($i = 0; ($row3 = grr_sql_row($res3, $i)); $i++)
@@ -2030,7 +2030,7 @@ function synchro_groupe($idGroupe, $action)
 		{
 			for ($i = 0; ($row2 = grr_sql_row($res2, $i)); $i++)
 			{
-				$sql3 = "SELECT ug.login FROM ".TABLE_PREFIX."_j_group_area jg LEFT JOIN ".TABLE_PREFIX."_utilisateurs_groupes ug ON jg.idgroupes = ug.idgroupes WHERE jg.id_area = $row2[0] AND jg.idgroupes = $idGroupe";
+				$sql3 = "SELECT DISTINCT ug.login FROM ".TABLE_PREFIX."_j_group_area jg LEFT JOIN ".TABLE_PREFIX."_utilisateurs_groupes ug ON jg.idgroupes = ug.idgroupes WHERE jg.id_area = $row2[0] AND jg.idgroupes = $idGroupe";
 				$res3 = grr_sql_query($sql3);
 
 				for ($i = 0; ($row3 = grr_sql_row($res3, $i)); $i++)
