8e3d2891e6c5357d636f57b379b06fd1c0b497ae
Yan <yan.naessens@gmail.com>
2025-11-26 16:37:40 +0100

    modification : autorisation donnée aux gestionnaires de la ressource pour gérer l'accès aux ressources restreintes

--- admin/controleurs/admin_book_room.php
+++ admin/controleurs/admin_book_room.php
@@ -1,10 +1,11 @@
 <?php
 /**
  * admin_book_room.php
- * Script de création/modification des ressources de l'application GRR
- * Dernière modification : $Date: 2024-06-17 17:09$
+ * Script gérant l'accès aux ressources restreintes de l'application GRR
+ * L'affichage est réalisé par admin_book_room.twig
+ * Dernière modification : $Date: 2025-11-26 16:23$
  * @author    JeromeB & Yan Naessens
- * @copyright Copyright 2003-2024 Team DEVOME - JeromeB
+ * @copyright Copyright 2003-2025 Team DEVOME - JeromeB
  * @link      http://www.gnu.org/licenses/licenses.html
  *
  * This file is part of GRR.
@@ -43,7 +44,7 @@ if ($test_user == "multi")
 {	
     if ($d['id_room'] != -1)
     {
-        if (authGetUserLevel(getUserName(), $d['id_room']) < 4)
+        if (authGetUserLevel(getUserName(), $d['id_room']) < 3)
         {
             showAccessDenied($back);
             exit();
@@ -75,7 +76,7 @@ if ($test_user == "simple")
 {
 	if ($d['id_room'] != -1)
 	{
-		if (authGetUserLevel(getUserName(), $d['id_room']) < 4)
+		if (authGetUserLevel(getUserName(), $d['id_room']) < 3)
 		{
 			showAccessDenied($back);
 			exit();
@@ -102,7 +103,7 @@ if ($test_user == "simple")
 
 if ($action=='del_user')
 {
-	if (authGetUserLevel(getUserName(), $d['id_room']) < 4)
+	if (authGetUserLevel(getUserName(), $d['id_room']) < 3)
 	{
 		showAccessDenied($back);
 		exit();
