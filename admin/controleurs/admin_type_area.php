05d382d63998387c32d1c36962fd01f146b86067
JeromeB <commercial@devome.com>
2026-01-24 12:14:37 +0100

    Sécurité + Passage popup en toast

--- admin/controleurs/admin_type_area.php
+++ admin/controleurs/admin_type_area.php
@@ -3,9 +3,9 @@
  * admin_type_area.php
  * interface de gestion des types de réservations pour un domaine
  * Ce script fait partie de l'application GRR
- * Dernière modification : $Date: 2017-12-16 14:00$
+ * Dernière modification : $Date: 2026-01-24 12:10$
  * @author    Laurent Delineau & JeromeB
- * @copyright Copyright 2003-2020 Team DEVOME - JeromeB
+ * @copyright Copyright 2003-2026 Team DEVOME - JeromeB
  * @link      http://www.gnu.org/licenses/licenses.html
  *
  * This file is part of GRR.
@@ -19,9 +19,8 @@
 $grr_script_name = "admin_type_area.php";
 
 // Initialisation
-$id_area = isset($_GET["id_area"]) ? $_GET["id_area"] : NULL;
+$id_area = isset($_GET["id_area"]) ? alphanum($_GET["id_area"]) : NULL;
 $types = array();
-$msg = "";
 
 check_access(4, $back);
 
@@ -41,17 +40,15 @@ get_vocab_admin("nobody");
 get_vocab_admin("save");
 get_vocab_admin("back");
 
-$trad['dDroitsAdmin'] = authGetUserLevel(getUserName(),-1);
-$trad['dIdArea'] = $id_area;
+$d['droitsAdmin'] = authGetUserLevel(getUserName(),-1);
+$d['idArea'] = $id_area;
 
 // Gestion du retour à la page précédente sans enregistrement
 if (isset($_GET['change_done']))
 {
-	Header("Location: "."?p=admin_room&id_area=".$_GET['id_area']);
+	Header("Location: "."?p=admin_room&id_area=".$id_area);
 	exit();
 }
-if ((isset($_GET['msg'])) && isset($_SESSION['displ_msg']) && ($_SESSION['displ_msg'] == 'yes') )
-	$msg = $_GET['msg'];
 
 $sql = "SELECT id, type_name, order_display, couleurhexa, type_letter, couleurtexte FROM ".TABLE_PREFIX."_type_area
 ORDER BY order_display, type_letter";
@@ -89,19 +86,23 @@ if (isset($_GET['valider']))
 	{
 		// Aucun type n'a été sélectionné. Dans ce cas, on impose au moins un type :
 		$del = grr_sql_query("DELETE FROM ".TABLE_PREFIX."_j_type_area WHERE id_area='".$id_area."' AND id_type = '".$type_si_aucun."'");
-		$msg = "Vous devez au définir au moins un type valide !";
+		$d['enregistrement'] = 3;
+		$d['msgToast'] = "Vous devez au définir au moins un type valide !";
+	}
+	else
+	{
+		$d['enregistrement'] = 1;
+		$d['msgToast'] = "Les modifications ont été enregistrées avec succès.";
 	}
 	// Type par défaut :
 	// On enregistre le nouveau type par défaut :
 	$reg_type_par_defaut = grr_sql_query("UPDATE ".TABLE_PREFIX."_area SET id_type_par_defaut='".$_GET['id_type_par_defaut']."' WHERE id='".$id_area."'");
 }
 
-affiche_pop_up($msg,"admin");
-
 //
 // Affichage
 //
-$trad['dNomDomaine'] = grr_sql_query1("SELECT area_name FROM ".TABLE_PREFIX."_area WHERE id='".$id_area."'");
+$d['nomDomaine'] = grr_sql_query1("SELECT area_name FROM ".TABLE_PREFIX."_area WHERE id='".$id_area."'");
 
 $res = grr_sql_query($sql);
 $nb_lignes = grr_sql_count($res);
@@ -117,7 +118,7 @@ if ($res && $nb_lignes > 0)
 		$types[] = array('id' => $row[0], 'type_letter' => $row[4], 'type_name' => $row[1], 'couleurhexa' => $row[3], 'couleurtexte' => $row[5], 'order_display' => $row[2], 'dispodomaine' => $dispoDomaine);
 	}
 
-	$trad['dDefautType'] = grr_sql_query1("SELECT id_type_par_defaut FROM ".TABLE_PREFIX."_area WHERE id = '".$id_area."'");
+	$d['defautType'] = grr_sql_query1("SELECT id_type_par_defaut FROM ".TABLE_PREFIX."_area WHERE id = '".$id_area."'");
 }
 
 
