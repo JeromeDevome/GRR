228f585d4683cd80c63b11d014fb89bdbf9926e0
JeromeB <commercial@devome.com>
2026-03-21 18:21:32 +0100

    New : Accès aux ressources à modérer pour les gestionnaires
    #474

--- reservation/controleurs/moderationsliste.php
+++ reservation/controleurs/moderationsliste.php
@@ -23,50 +23,41 @@ $trad = $vocab;
 
 $acces = false;
 $listeModeration = array();
-$res = false;
-if (authGetUserLevel($d['gNomUser'],-1) > 5) // admin général
-{
-	$acces = true;
-	$sql = "SELECT e.id,r.room_name,e.start_time,create_by,beneficiaire FROM ".TABLE_PREFIX."_entry e JOIN ".TABLE_PREFIX."_room r ON e.room_id = r.id WHERE e.moderate = 1 AND e.supprimer = 0 ";
-	$res = grr_sql_query($sql);
-}
-elseif (isset($_GET['id_site']) && (authGetUserLevel($d['gNomUser'],intval($_GET['id_site']),'site') > 4)) // admin du site
-{
-	$acces = true;
-	$sql = "SELECT e.id,r.room_name,e.start_time,create_by,beneficiaire FROM ".TABLE_PREFIX."_entry e JOIN ".TABLE_PREFIX."_room r ON e.room_id = r.id JOIN ".TABLE_PREFIX."_j_site_area j ON r.area_id = j.id_area WHERE (j.id_site = ".intval($_GET['id_site'])." AND e.moderate = 1  AND e.supprimer = 0)";
-	$res = grr_sql_query($sql);
-}
-elseif (isset($_GET['area']) && (authGetUserLevel($d['gNomUser'],intval($_GET['area']),'area') > 3)) // admin du domaine
-{
-	$acces = true;
-	$sql = "SELECT e.id,r.room_name,e.start_time,create_by,beneficiaire FROM ".TABLE_PREFIX."_entry e JOIN ".TABLE_PREFIX."_room r ON e.room_id = r.id JOIN ".TABLE_PREFIX."_area a ON r.area_id = a.id WHERE (a.id = ".intval($_GET['area'])." AND e.moderate = 1 AND e.supprimer = 0)";
-	$res = grr_sql_query($sql);
-}
-elseif (isset($_GET['room']) && (authGetUserLevel($d['gNomUser'],intval($_GET['room']),'room') > 2)) // gestionnaire de la ressource
-{
-	$acces = true;
-	$sql = "SELECT e.id,r.room_name,e.start_time,create_by,beneficiaire FROM ".TABLE_PREFIX."_entry e JOIN ".TABLE_PREFIX."_room r ON e.room_id = r.id WHERE (e.moderate = 1 AND e.supprimer = 0 AND e.room_id = ".intval($_GET['room']).") ";
-	$res = grr_sql_query($sql);
-}
 
-if ($acces)
-{
-	$d['acces'] = 1;
-	if ($res)
-	{
-		if ($sql != ""){
-			$res = grr_sql_query($sql);
-			if ($res)
-			{
-				foreach($res as $row) 
-				{
-					$link = "?p=vuereservation&id=".$row['id']."&mode=page";
-					$listeModeration[] = array('ressource' => $row['room_name'], 'debut' => time_date_string($row['start_time'], $dformat), 'createur' => $row['create_by'], 'beneficiaire' => $row['beneficiaire'], 'lien' => $link );
-				}
-			}
-		}
-		$d['nbResaAModerer'] = count($listeModeration);
-	}
+// Utiliser la fonction resaToModerate() optimisée
+$resasAModerer = resaToModerate($d['gNomUser']);
+
+if (!empty($resasAModerer)) {
+    $acces = true;
+    $d['acces'] = 1;
+
+    foreach($resasAModerer as $resa) {
+        $link = "?p=vuereservation&id=".$resa['id']."&mode=page";
+        if (Settings::get("module_multisite") == "Oui")
+        {
+            $listeModeration[] = array(
+                'site' => $resa['site'],
+                'ressource' => $resa['room'],
+                'debut' => time_date_string($resa['start_time'], $dformat),
+                'createur' => $resa['create_by'],
+                'beneficiaire' => $resa['beneficiaire'],
+                'lien' => $link
+            );
+        }
+        else
+        {
+            $listeModeration[] = array(
+                'domaine' => $resa['area'],
+                'ressource' => $resa['room'],
+                'debut' => time_date_string($resa['start_time'], $dformat),
+                'createur' => $resa['create_by'],
+                'beneficiaire' => $resa['beneficiaire'],
+                'lien' => $link
+            );
+        }
+    }
+
+    $d['nbResaAModerer'] = count($listeModeration);
 }
 
 
