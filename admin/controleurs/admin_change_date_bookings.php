ade52211bd84bc4a2390f5a8356c63e3369d8557
JeromeB <commercial@devome.com>
2026-06-06 15:18:17 +0200

    Renforcement sécurité changement de dates

--- admin/controleurs/admin_change_date_bookings.php
+++ admin/controleurs/admin_change_date_bookings.php
@@ -20,9 +20,19 @@ $grr_script_name = "admin_change_date_bookings.php";
 
 SecuAccess::CheckAccess(6, $back);
 
-if (isset($_GET['valid']) && ($_GET['valid'] == "yes"))
+// les variables attendues et leur type
+$form_vars = array(
+    'valid' => 'int',
+    'begin_bookings' => 'int',
+    'end_bookings' => 'int'
+);
+// récupération des valeurs des variables passées en paramètres
+foreach($form_vars as $var => $var_type)
+    $$var = SecuChaine::GetFormVarSecure($var, $var_type);
+
+if ($valid == 1)
 {
-	if (!Settings::set("begin_bookings", $_GET['begin_bookings']))
+	if (!Settings::set("begin_bookings", $begin_bookings))
 		echo "Erreur lors de l'enregistrement de begin_bookings !<br />";
 	else
 	{
@@ -31,7 +41,7 @@ if (isset($_GET['valid']) && ($_GET['valid'] == "yes"))
 		$del = grr_sql_query("DELETE FROM ".TABLE_PREFIX."_entry_moderate WHERE (end_time < ".Settings::get('begin_bookings').")");
 		$del = grr_sql_query("DELETE FROM ".TABLE_PREFIX."_calendar WHERE DAY < ".Settings::get("begin_bookings"));
 	}
-	if (!Settings::set("end_bookings", $_GET['end_bookings']))
+	if (!Settings::set("end_bookings", $end_bookings))
 		echo "Erreur lors de l'enregistrement de end_bookings !<br />";
 	else
 	{
@@ -43,19 +53,11 @@ if (isset($_GET['valid']) && ($_GET['valid'] == "yes"))
 	header("Location: ?p=admin_page_reservation");
 
 }
-else if (isset($_GET['valid']) && ($_GET['valid'] == "no"))
-	header("Location: ?p=admin_page_reservation");
-
-
-get_vocab_admin("admin_confirm_change_date_bookings");
-get_vocab_admin("msg_del_bookings");
-get_vocab_admin("save");
-get_vocab_admin("cancel");
 
 //data
-$trad['dBegin_bookings']	= $_GET['begin_bookings'];
-$trad['dEnd_bookings']		= $_GET['end_bookings'];
-$trad['dPlageSelectionner']	= date("d/m/Y", $_GET['begin_bookings'])." - ". date("d/m/Y", $_GET['end_bookings']);
+$trad['dBegin_bookings']	= $begin_bookings;
+$trad['dEnd_bookings']		= $end_bookings;
+$trad['dPlageSelectionner']	= date("d/m/Y", $begin_bookings)." - ". date("d/m/Y", $end_bookings);
 
 echo $twig->render($page.'.twig', array('liensMenu' => $menuAdminT, 'liensMenuN2' => $menuAdminTN2, 'd' => $d, 'trad' => $trad, 'settings' => $AllSettings));
 ?>
