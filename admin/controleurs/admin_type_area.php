8ed2c32af692508fab2f32c62a723cce01605502
JeromeB <commercial@devome.com>
2026-09-26 18:03:22 +0200

    Refonte type par domaine
    Amélioration affichage
    Sécurité
    Gestion d'erreur si type par défaut est invalide

--- admin/controleurs/admin_type_area.php
+++ admin/controleurs/admin_type_area.php
@@ -3,7 +3,7 @@
  * admin_type_area.php
  * interface de gestion des types de réservations pour un domaine
  * Ce script fait partie de l'application GRR
- * Dernière modification : $Date: 2026-01-27 09:57$
+ * Dernière modification : $Date: 2026-09-26 17:00$
  * @author    Laurent Delineau & JeromeB
  * @copyright Since 2003 Team DEVOME - JeromeB
  * @link      http://www.gnu.org/licenses/licenses.html
@@ -18,107 +18,104 @@
 
 $grr_script_name = "admin_type_area.php";
 
-// Initialisation
-$id_area = isset($_GET["id_area"]) ? intval($_GET["id_area"]) : NULL; // id_area est un entier
-$types = array();
-
+// Accès à la page
 SecuAccess::CheckAccess(4, $back);
 
-get_vocab_admin("admin_type");
-get_vocab_admin("display_add_type");
-get_vocab_admin("explications_active_type");
 
-get_vocab_admin("type_num");
-get_vocab_admin("type_name");
-get_vocab_admin("type_color");
-get_vocab_admin("type_order");
-get_vocab_admin("type_valide_domaine");
-get_vocab_admin("type_par_defaut");
+// les variables attendues et leur type
+$form_vars = array(
+    'p_id_area' => array('int', 0),
+	'p_id_type_par_defaut' => array('int', -1),
+	'p_submit' => array('int', 0),
+	'p_types' => array('array', array()), // tableau des types sélectionnés pour le domaine
+);
+// récupération des valeurs des variables passées en paramètres
+foreach($form_vars as $var => $params)
+    $$var = SecuChaine::GetFormVarSecure($var, $params[0], $params[1]);
 
-get_vocab_admin("nobody");
-
-get_vocab_admin("save");
-get_vocab_admin("back");
-
-$d['droitsAdmin'] = SecuAccess::UserLevel(getUserName(),-1);
-$d['idArea'] = $id_area;
-
-// Gestion du retour à la page précédente sans enregistrement
-if (isset($_GET['change_done']))
-{
-	Header("Location: "."?p=admin_room&id_area=".$id_area);
-	exit();
-}
-
-$sql = "SELECT id, type_name, order_display, couleurhexa, type_letter, couleurtexte FROM ".TABLE_PREFIX."_type_area
-ORDER BY order_display, type_letter";
-
-//
-// Enregistrement
-//
-if (isset($_GET['valider']))
-{
-	$res = grr_sql_query($sql);
-	$nb_types_valides = 0;
-	if ($res)
+/** Actions **/
+	if($p_submit == 1)
 	{
-		foreach($res as $row)
+		$typesSelect = array_unique(array_map('strval', array_map(array('SecuChaine', 'Numeric'), $p_types)));
+		$nb_Select = count($typesSelect);
+		$typesInvalide = array();
+		$sql = "SELECT id FROM ".TABLE_PREFIX."_type_area";
+		$res = grr_sql_query($sql);
+		$typeDefaut = $p_id_type_par_defaut;
+		$erreurTypeDefaut = false;
+
+		if ($res)
 		{
-			if (isset($_GET[$row['id']]))
+			foreach ($res as $row)
 			{
-				$nb_types_valides ++;
-				$del = grr_sql_query("DELETE FROM ".TABLE_PREFIX."_j_type_area WHERE id_area='".$id_area."' AND id_type = '".$row['id']."'");
+				if (!in_array((string) $row['id'], $typesSelect, true))
+					$typesInvalide[] = $row['id'];
 			}
-			else
+		}
+
+		if ($nb_Select > 0)
+		{
+			// On supprime tout avant d'insérer les nouveaux
+			$del = grr_sql_query("DELETE FROM ".TABLE_PREFIX."_j_type_area WHERE id_area='".$p_id_area."'");
+
+			foreach ($typesInvalide as $typeI)
 			{
-				$type_si_aucun = $row['id'];
-				$test = grr_sql_query1("SELECT count(id_type) FROM ".TABLE_PREFIX."_j_type_area WHERE id_area = '".$id_area."' AND id_type = '".$row['id']."'");
-				if ($test == 0)
+				$sql1 = "INSERT INTO ".TABLE_PREFIX."_j_type_area SET id_area='".$p_id_area."', id_type = '".$typeI."'";
+				if (grr_sql_command($sql1) < 0)
+					fatal_error(1, "<p>" . grr_sql_error());
+
+				if($typeI == $typeDefaut) // On n'accepte pas que le type par défaut soit pas valide
 				{
-					$sql1 = "INSERT INTO ".TABLE_PREFIX."_j_type_area SET id_area='".$id_area."', id_type = '".$row['id']."'";
-					if (grr_sql_command($sql1) < 0)
-						fatal_error(1, "<p>" . grr_sql_error());
+					$erreurTypeDefaut = true;
+					$typeDefaut = -1;
 				}
 			}
 		}
+
+		if ($nb_Select == 0)
+		{
+			$d['enregistrement'] = 3;
+			$d['msgToast'] = get_vocab("def_type_non_valide");
+		}
+		elseif($erreurTypeDefaut == true)
+		{
+			$d['enregistrement'] = 2;
+			$d['msgToast'] = "La valeur par défaut doit-être un type valide pour le domaine !";
+		}
+		else
+		{
+			$d['enregistrement'] = 1;
+			$d['msgToast'] = get_vocab('modify_succeed');
+		}
+
+		// On enregistre le nouveau type par défaut :
+		$reg_type_par_defaut = grr_sql_query("UPDATE ".TABLE_PREFIX."_area SET id_type_par_defaut='".$typeDefaut."' WHERE id='".$p_id_area."'");
 	}
-	if ($nb_types_valides == 0)
+
+
+/** Affichage de la page **/
+	$trad['TitrePage']		= $trad["admin_type"];
+	$trad['SousTitrePage']	= grr_sql_query1("SELECT area_name FROM ".TABLE_PREFIX."_area WHERE id='".$p_id_area."'");
+
+	$types				= array();
+	$d['idArea']		= $p_id_area;
+	$d['droitsAdmin']	= SecuAccess::UserLevel(getUserName(),-1);
+
+	$sql = "SELECT id, type_name, order_display, couleurhexa, type_letter, couleurtexte FROM ".TABLE_PREFIX."_type_area ORDER BY order_display, type_letter";
+	$res = grr_sql_query($sql);
+	$nb_lignes = grr_sql_count($res);
+
+	if ($res && $nb_lignes > 0)
 	{
-		// Aucun type n'a été sélectionné. Dans ce cas, on impose au moins un type :
-		$del = grr_sql_query("DELETE FROM ".TABLE_PREFIX."_j_type_area WHERE id_area='".$id_area."' AND id_type = '".$type_si_aucun."'");
-		$d['enregistrement'] = 3;
-		$d['msgToast'] = get_vocab("def_type_non_valide");
-	}
-	else
-	{
-		$d['enregistrement'] = 1;
-		$d['msgToast'] = get_vocab('modify_succeed');
-	}
-	// Type par défaut :
-	// On enregistre le nouveau type par défaut :
-	$reg_type_par_defaut = grr_sql_query("UPDATE ".TABLE_PREFIX."_area SET id_type_par_defaut='".SecuChaine::ProtectDataSql($_GET['id_type_par_defaut'])."' WHERE id='".$id_area."'");
-}
+		foreach($res as $row)
+		{
+			$dispoDomaine = grr_sql_query1("SELECT count(id_type) FROM ".TABLE_PREFIX."_j_type_area WHERE id_area = '".$p_id_area."' AND id_type = '".$row['id']."'");
 
-//
-// Affichage
-//
-$d['nomDomaine'] = grr_sql_query1("SELECT area_name FROM ".TABLE_PREFIX."_area WHERE id='".$id_area."'");
+			$types[] = array('id' => $row['id'], 'type_letter' => $row['type_letter'], 'type_name' => $row['type_name'], 'couleurhexa' => $row['couleurhexa'], 'couleurtexte' => $row['couleurtexte'], 'order_display' => $row['order_display'], 'dispodomaine' => $dispoDomaine);
+		}
 
-$res = grr_sql_query($sql);
-$nb_lignes = grr_sql_count($res);
-
-if ($res && $nb_lignes > 0)
-{
-	foreach($res as $row)
-	{
-		$dispoDomaine = grr_sql_query1("SELECT count(id_type) FROM ".TABLE_PREFIX."_j_type_area WHERE id_area = '".$id_area."' AND id_type = '".$row['id']."'");
-
-		$types[] = array('id' => $row['id'], 'type_letter' => $row['type_letter'], 'type_name' => $row['type_name'], 'couleurhexa' => $row['couleurhexa'], 'couleurtexte' => $row['couleurtexte'], 'order_display' => $row['order_display'], 'dispodomaine' => $dispoDomaine);
+		$d['defautType'] = grr_sql_query1("SELECT id_type_par_defaut FROM ".TABLE_PREFIX."_area WHERE id = '".$p_id_area."'");
 	}
 
-	$d['defautType'] = grr_sql_query1("SELECT id_type_par_defaut FROM ".TABLE_PREFIX."_area WHERE id = '".$id_area."'");
-}
-
-
-echo $twig->render('admin_type_area.twig', array('liensMenu' => $menuAdminT, 'liensMenuN2' => $menuAdminTN2, 'd' => $d, 'trad' => $trad, 'settings' => $AllSettings, 'types' => $types));
+	echo $twig->render('admin_type_area.twig', array('liensMenu' => $menuAdminT, 'liensMenuN2' => $menuAdminTN2, 'd' => $d, 'trad' => $trad, 'settings' => $AllSettings, 'types' => $types));
 ?>
