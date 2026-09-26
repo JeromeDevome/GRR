7ab1df9f21066be36bca521e2bc1e179eb0c1317
JeromeB <commercial@devome.com>
2026-09-26 16:13:21 +0200

    Refonte édition groupe
    Amélioration affichage
    Sécuritité
    Rappel des informations en cas d'erreur dans le formulaire

--- admin/controleurs/admin_groupe_edit.php
+++ admin/controleurs/admin_groupe_edit.php
@@ -3,8 +3,8 @@
  * admin_groupe_edit.php
  * interface de gestion des utilisateurs de l'application GRR
  * Ce script fait partie de l'application GRR
- * Dernière modification : $Date: 2022-05-18 22:00$
- * @author    Laurent Delineau & JeromeB
+ * Dernière modification : $Date: 2026-09-26 12:00$
+ * @author    JeromeB
  * @copyright Since 2003 Team DEVOME - JeromeB
  * @link      http://www.gnu.org/licenses/licenses.html
  *
@@ -19,130 +19,116 @@
 
 $grr_script_name = "admin_groupe_edit.php";
 
+// Accès à la page
 if ((SecuAccess::UserLevel(getUserName(), -1) < 6) && (SecuAccess::UserLevel(getUserName(), -1, 'user') !=  1))
 {
 	showAccessDenied($back);
 	exit();
 }
 
-$idgroupe = isset($_GET["idgroupe"]) && $_GET["idgroupe"] != '' ? $_GET["idgroupe"] : 0;
-$valid = isset($_GET["valid"]) ? $_GET["valid"] : NULL;
+// les variables attendues et leur type
+$form_vars = array(
+    'p_idgroupe' => array('int', 0),
+	'p_submit' => array('int', 0),
+	'p_nom' => array('string', ''),
+	'p_description' => array('string', ''),
+	'p_archive' => array('int', 0)
 
-$user_nom = '';
-$user_description= '';
-$user_archive = '';
-$display = "";
-$retry = '';
+);
+// récupération des valeurs des variables passées en paramètres
+foreach($form_vars as $var => $params)
+    $$var = SecuChaine::GetFormVarSecure($var, $params[0], $params[1]);
+
 
 $groupe = array();
+$retry = false;
 
-if ($valid == "yes")
-{
-	// Restriction dans le cas d'une démo
-	VerifyModeDemo();
-	$reg_nom = isset($_GET["reg_nom"]) ? $_GET["reg_nom"] : NULL;
-	$reg_description = isset($_GET["reg_description"]) ? $_GET["reg_description"] : NULL;
-	$reg_archive= isset($_GET["reg_archive"]) ? $_GET["reg_archive"] : "off";
-
-	if($reg_archive == "on")
-		$archive = 1;
-	else
-		$archive = 0;
-
-	if ($reg_nom == '')
+/** Actions **/
+	if($p_submit == 1)
 	{
-		$d['enregistrement'] = 3;
-		$retry = 'yes';
-	}
-	else
-	{
-		// Création d'un groupe
-		if ($idgroupe == 0)
+		VerifyModeDemo();
+
+		if ($p_nom == "") // Nom du groupe obligatoire
 		{
-
-			$sql = "SELECT * FROM ".TABLE_PREFIX."_groupes WHERE nom = '".$reg_nom."'";
-			$res = grr_sql_query($sql);
-			$nombreligne = grr_sql_count ($res);
-			if ($nombreligne != 0)
-			{
-				$d['enregistrement'] = 2;
-				$retry = 'yes';
-			}
-			else
-			{
-				$sql = "INSERT INTO ".TABLE_PREFIX."_groupes SET
-				nom='".SecuChaine::ProtectDataSql($reg_nom)."',
-				description='".SecuChaine::ProtectDataSql($reg_description)."',
-				archive='".SecuChaine::ProtectDataSql($archive)."'";
-				if (grr_sql_command($sql) < 0)
-					fatal_error(0, get_vocab("msg_login_created_error") . grr_sql_error());
-				else
-					$d['enregistrement'] = 1;
-			}
+			$d['enregistrement'] = 3;
+			$retry = true;
 		}
-		// Modification d'un groupe
 		else
 		{
-			$sql = "SELECT * FROM ".TABLE_PREFIX."_groupes WHERE nom = '".$reg_nom."' AND idgroupes <> '".$idgroupe."'";
-			$res = grr_sql_query($sql);
-			$nombreligne = grr_sql_count ($res);
-			if ($nombreligne != 0)
+			if ($p_idgroupe == 0) // Création
 			{
-				$d['enregistrement'] = 2;
-				$retry = 'yes';
-			}
-			else
-			{
-				$sql = "UPDATE ".TABLE_PREFIX."_groupes SET nom='".SecuChaine::ProtectDataSql($reg_nom)."',
-				description='".SecuChaine::ProtectDataSql($reg_description)."',
-				archive='".SecuChaine::ProtectDataSql($archive)."'
-				WHERE idgroupes='".SecuChaine::ProtectDataSql($idgroupe)."'";
-				if (grr_sql_command($sql) < 0)
-					fatal_error(0, get_vocab("message_records_error") . grr_sql_error());
+				$sql = "SELECT * FROM ".TABLE_PREFIX."_groupes WHERE nom = '".$p_nom."'";
+				$res = grr_sql_query($sql);
+				$nombreligne = grr_sql_count ($res);
+				if ($nombreligne != 0)
+				{
+					$d['enregistrement'] = 2;
+					$retry = true;
+				}
 				else
-					$d['enregistrement'] = 1;
+				{
+					$sql = "INSERT INTO ".TABLE_PREFIX."_groupes SET
+					nom='".SecuChaine::ProtectDataSql($p_nom)."',
+					description='".SecuChaine::ProtectDataSql($p_description)."',
+					archive='".SecuChaine::ProtectDataSql($p_archive)."'";
+					if (grr_sql_command($sql) < 0)
+						fatal_error(0, get_vocab("msg_login_created_error") . grr_sql_error());
+					else
+						$d['enregistrement'] = 1;
+				}
 			}
+			else // Modification
+			{
+				$sql = "SELECT * FROM ".TABLE_PREFIX."_groupes WHERE nom = '".$p_nom."' AND idgroupes <> '".$p_idgroupe."'";
+				$res = grr_sql_query($sql);
+				$nombreligne = grr_sql_count ($res);
+				if ($nombreligne != 0)
+				{
+					$d['enregistrement'] = 2;
+					$retry = 'yes';
+				}
+				else
+				{
+					$sql = "UPDATE ".TABLE_PREFIX."_groupes SET nom='".SecuChaine::ProtectDataSql($p_nom)."',
+					description='".SecuChaine::ProtectDataSql($p_description)."',
+					archive='".SecuChaine::ProtectDataSql($p_archive)."'
+					WHERE idgroupes='".SecuChaine::ProtectDataSql($p_idgroupe)."'";
+					if (grr_sql_command($sql) < 0)
+						fatal_error(0, get_vocab("message_records_error") . grr_sql_error());
+					else
+						$d['enregistrement'] = 1;
+				}
+			}
+
 		}
 	}
-	if ($retry == 'yes')
+
+
+/** Affichage de la page **/
+	$trad['TitrePage']	= $trad['admin_groupe'];
+
+	// On appelle les informations du groupe pour les afficher
+	if ($p_idgroupe != 0)
 	{
-		$user_nom = $reg_nom;
-		$user_description = $reg_description;
-		$user_archive = $reg_archive;
+		$res = grr_sql_query("SELECT idgroupes, nom, description, archive FROM ".TABLE_PREFIX."_groupes WHERE idgroupes='$p_idgroupe'");
+		if (!$res)
+			fatal_error(0, get_vocab('message_records_error'));
+		$groupe = grr_sql_row_keyed($res, 0);
+		grr_sql_free($res);
+
+		$trad['SousTitrePage'] = get_vocab('change');
 	}
-}
-// On appelle les informations de l'utilisateur pour les afficher :
-if (isset($idgroupe) && ($idgroupe != 0))
-{
-	$res = grr_sql_query("SELECT idgroupes, nom, description, archive FROM ".TABLE_PREFIX."_groupes WHERE idgroupes='$idgroupe'");
-	if (!$res)
-		fatal_error(0, get_vocab('message_records_error'));
-	$groupe = grr_sql_row_keyed($res, 0);
-	grr_sql_free($res);
-}
-if ((SecuAccess::UserLevel(getUserName(), -1) < 1) && (Settings::get("authentification_obli") == 1))
-{
-	showAccessDenied($back);
-	exit();
-}
+	else
+	{
+		$trad['SousTitrePage'] = get_vocab('add');
+	}
 
+	if($retry == true)
+	{
+		$groupe["nom"] = $p_nom;
+		$groupe["description"] = $p_description;
+		$groupe["archive"] = $p_archive;
+	}
 
-$trad['dDisplay'] = $display;
-
-get_vocab_admin('groupe_add');
-get_vocab_admin('groupe_modifier');
-
-get_vocab_admin("name");
-get_vocab_admin("description");
-get_vocab_admin("archiver");
-
-
-get_vocab_admin("groupe_exist");
-get_vocab_admin("please_enter_name");
-get_vocab_admin("message_records");
-
-get_vocab_admin("back");
-get_vocab_admin("save");
-
-echo $twig->render('admin_groupe_edit.twig', array('liensMenu' => $menuAdminT, 'liensMenuN2' => $menuAdminTN2, 'd' => $d, 'trad' => $trad, 'settings' => $AllSettings, 'groupe' => $groupe));
+	echo $twig->render('admin_groupe_edit.twig', array('liensMenu' => $menuAdminT, 'liensMenuN2' => $menuAdminTN2, 'd' => $d, 'trad' => $trad, 'settings' => $AllSettings, 'groupe' => $groupe));
 ?>
